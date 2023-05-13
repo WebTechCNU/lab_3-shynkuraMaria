5249066cd8fd8815a90a4c8bd9080167caaed8ae
shynkuraMaria <132786247+shynkuraMaria@users.noreply.github.com>
2023-05-13 22:03:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,302 @@
+let arr = [
+    {
+        name: "telescope",
+        link: "images/pic1.jpg",
+        description: "Skywatcher",
+        description_2: "Telescope N 200/1000 Explorer 200P EQ5",
+        price: "€ 899.00",
+        category: 1
+    },
+    {
+        name: "telescope 2",
+        link: "images/pic2.jpg",
+        description: "Celestron",  
+        description_2: "Schmidt-Cassegrain telescope SC 203/2032",
+        price: "€ 3,099.00",
+        category: 1
+    },
+    {
+        name: "telescope 3",
+        link: "images/pic3.jpg",
+        description: "Omegon",
+        description_2: "Advanced Telescope 150/750 EQ-320",
+        price: "€ 429.00",
+        category: 1
+    },
+    {
+        name: "telescope 4",
+        link: "images/pic4.jpg",
+        description: "Omegon",  
+        description_2: "Advanced Telescope 130 EQ-320",
+        price: "€ 319.00",
+        category: 1
+    },
+    {
+        name: "telescope 5",
+        link: "images/pic5.jpg",
+        description: "Omegon",
+        description_2: "Telescope AC 60/700 AZ-1",
+        price: "€ 99.00",
+        category: 1
+    },
+    {
+        name: "telescope 6",
+        link: "images/pic6.jpg",
+        description: "Omegon",
+        description_2: "Telescope N 76/700 AZ-1",
+        price: "€ 119.00",
+        category: 1
+    },
+    {
+        name: "telescope 7",
+        link: "images/pic7.jpg",
+        description: "Celestron",
+        description_2: "Schmidt-Cassegrain SC 150/1500 NexStar Evolution 6",
+        price: "€ 2,199.00",
+        category: 1
+    },
+    {
+        name: "telescope 8",
+        link: "images/pic8.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope ProDob N 254/1250 DOB II",
+        price: "€ 999.00",
+        category: 1
+    },
+    {
+        name: "telescope 9",
+        link: "images/pic9.jpg",
+        description: "Omegon",
+        description_2: "Telescope AC 70/400 Solar BackPack AZ",
+        price: "€ 119.00",
+        category: 1
+    },
+    {
+        name: "telescope 10",
+        link: "images/pic10.jpg",
+        description: "Omegon",
+        description_2: "Telescope AC 50/600 AZ",
+        price: "€ 79.00",
+        category: 1
+    },
+    {
+        name: "telescope 11",
+        link: "images/pic11.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 1
+    },
+    {
+        name: "mounting",
+        link: "images/picc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 2
+    },
+    {
+        name: "accessories",
+        link: "images/picccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 4
+    },
+    {
+        name: "mounting",
+        link: "images/picc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 2
+    },
+    {
+        name: "mounting",
+        link: "images/picc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 2
+    },
+    {
+        name: "mounting",
+        link: "images/picc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 2
+    },
+    {
+        name: "filters",
+        link: "images/piccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 3
+    },
+    {
+        name: "filters",
+        link: "images/piccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 3
+    },
+    {
+        name: "filters",
+        link: "images/piccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 3
+    },
+    {
+        name: "accessories",
+        link: "images/picccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 4
+    },
+    {
+        name: "accessories",
+        link: "images/picccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 4
+    },
+    {
+        name: "accessories",
+        link: "images/picccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 4
+    },
+    {
+        name: "filters",
+        link: "images/piccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 3
+    },
+    {
+        name: "filters",
+        link: "images/piccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 3
+    },
+    {
+        name: "accessories",
+        link: "images/picccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 4
+    },
+    {
+        name: "mounting",
+        link: "images/picc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 2
+    },
+    {
+        name: "mounting",
+        link: "images/picc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 2
+    },
+    {
+        name: "telescope 12",
+        link: "images/pic12.jpg",
+        description: "Skywatcher",
+        description_2: "Maksutov telescope MC 127/1500 SkyMax-127 AZ-GTi GoTo WiFi",
+        price: "€ 2,199.00",
+        category: 1
+    },
+    {
+        name: "accessories",
+        link: "images/picccc1.jpg",
+        description: "Omegon",
+        description_2: "Dobson telescope N 76/300 DOB",
+        price: "€ 84.00",
+        category: 4
+    },];
+let cookie = "user subscribed";
+
+function getElements(category) {
+    let cont = document.querySelector("#container");
+    let out = "<div class='row'>";
+    let i = 0;
+    arr.forEach(elem => {
+        if (category == null || elem.category == category) {
+            out += "<div class='product-list'><img class='product-item product-img' src=" + elem.link + " alt=" + elem.name + " ><h2 class='product-list'>" + elem.description + "</h2> <h3 class='product-list'>" + elem.description_2 + "</h3> <span class='price'><span class='price'>"+ elem.price +"</span> </div>"
+            i++;
+        }
+        if (i % 3 == 0 && 1) {
+            out += "</div><div class='row'>";
+        }
+    });
+    out += "</div>";
+    cont.innerHTML = out;
+}
+
+let modal = document.getElementById("myModal");
+let closeBtn = document.querySelector("#closeBtn");
+
+function close() {
+    modal.style.display = "none";
+}
+
+setTimeout(() => {
+    if (document.cookie != cookie) {
+        modal.style.display = "block";
+
+    }
+}, 3000)
+
+let closeTime = 5;
+
+setInterval(() => {
+    if (modal.style.display == "block" && closeTime > 0) {
+        closeTime -= 1;
+        closeBtn.innerHTML = closeTime + " seconds";
+    } else if(closeTime <= 0){
+        closeBtn.innerHTML = "&times;";
+        closeBtn.addEventListener("click", close);
+    }
+}, 1000)
+
+
+function subscribe() {
+    document.cookie = cookie;
+    close();
+}
+
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
